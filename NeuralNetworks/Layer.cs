38291945a263ac19b74da9c94f75174f498cfd3a
Roland Völker <roland_voelker@web.de>
2019-11-19 21:20:07 +0100

    updated diagram

--- NeuralNetworks/Layer.cs
+++ NeuralNetworks/Layer.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using VectorMath;
+﻿using VectorMath;
 
 namespace NeuralNetworks
 {
-    class Layer
+    public class Layer
     {
-        Vector vector;
+        private Vector input;
+        private Vector output;
+
+        public Layer()
+        {
+            //TODO: Assign random values to Weights
+        }
+
+        public Vector Input => input;
+
+        public Vector Output => output;
+
+        public Matrix Weights { get; set; }
+
+        public Vector Feed(Vector rawInput)
+        {
+            // Extend the rawInput by one 1 at the end
+            input = new Vector(rawInput.Dim + 1);
+
+            for (int i = 0; i < input.Dim; i++)
+                input[i] = rawInput[i];
+
+            input[rawInput.Dim] = 1;
+            //////////////////////////////////////////
+
+            // Calculate the output vector by multiplicating with weights
+            output = Weights * input;
+
+            return output;
+        }
+
+        public Vector CalcDelta(Layer next)
+        {
+
+        }
+
+        public Vector CalcDelta(Vector netOutput)
+        {
+
+        }
+
+
     }
 }
